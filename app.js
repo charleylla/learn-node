b3651e107cf07de489383f432ca4557c17a74bfb
Charley <charleylla@foxmail.com>
2017-10-10 18:01:47 +0800

    use body-parser middleware to handle post requests

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/",(req,res) =>{
-    /**
-     * Foramt a queryString from url
-     * for example,to open a request such as "localhost:8080?name=charley&age=23"
-     * and the value of req.query is {name:"charley",age:"23"}
-     * with this property you don't need to parse the queryString from url by yourself
-     * and express has done it.
-     */
-    console.log(req.query)
-    /**
-     * you can get certain query value like this
-     */
-    console.log(req.query.name)
-    res.json({code:0,status:200})
-})
+/**
+ * import body-parser middleware
+ * this middleware helps to req.body property
+ * if you don't use this middleware,the value of req.body is undefined
+ * this middleware helps you to handle post requests 
+ */
+const bodyParser = require("body-parser");
+/**
+ * use body-parser middleware
+ * you should call app.use method
+ * in this example,bodyParser.json is used to parse mutlipart/form-data
+ * and bodyParser.urlencoded is used to parse application/x-www-form-urlencoded
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/:name",(req,res) =>{
-    /**
-     * this property is used to format the path from url
-     * for example,if you open a request "localhost:8080/charley",the path of this url is "charley"
-     * and the value of req.params is {name:"charley"} 
-     * similar to req.query,you don't need to parse the path from url by yourself.
-     */
-    console.log(req.params)
-    /**
-     * you can get certain path value like this.
-     */
-    console.log(req.params.name)
+app.post("/upload",(req,res) =>{
+    console.log(req.body)
     res.json({code:0,status:200})
 })
 
