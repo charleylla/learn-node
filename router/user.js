6727d220df13ead232b7feb1b2a681ef7f5e94a1
Charley <charleylla@foxmail.com>
2017-10-11 11:30:39 +0800

    使用 route 方法构建 restful 风格的 API

--- router/user.js
+++ router/user.js
@@ -2,30 +2,29 @@
 const express = require("express")
 // 创建一个 router 实例，所有的请求都挂载到这个实例上
 const router = express.Router();
+let charley = {name:"charley",age:23};
 
 /**
- * 这里的 / 表示的不是根路径，而是相对路径
- * 代表了前面的 /user
- * 如果不定义 / 的话，是无法访问到 /user 的，之能访问到被定义的路径
+ * 也可以使用 route 方法接受请求，然后根据不同的请求类型进行处理，方便做出 restful 风格的 api
  */
-router.get("/",(req,res,next) =>{
-    res.status(200)
-    const users = [{
-        name:"charley",
-        age:23
-    },{
-        name:"lylla",
-        age:23
-    }]
-    res.send(JSON.stringify(users))
-})
-
-router.get("/charley",(req,res,next) =>{
-    res.status(200)
-    res.json({
-        name:"Charley",
-        age:23
+router.route("/charley")
+    // 使用 get 请求时获取人物信息
+    .get((req,res,next) =>{
+        res.json({
+            code:0,
+            msg:"success",
+            user:charley
+        })
     })
-})
+    // 使用 post 请求时修改人物信息
+    .post((req,res,next) =>{
+        const { name,age } = req.body;
+        charley.name = name;
+        charley.age = age;
+        res.json({
+            code:0,
+            msg:"success"
+        })
+    })
 
 module.exports = router;
